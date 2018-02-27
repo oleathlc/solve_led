a09224011588c6120415ad0c5e6c35b62e99c8e9
oleathlc <33549380+oleathlc@users.noreply.github.com>
2018-02-27 22:18:27 +0000

    Basic structure added to the main file (solve_led_project.py)

--- solve_led_project/solve_led_project.py
+++ solve_led_project/solve_led_project.py
@@ -1,3 +1,35 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+def main(file, N):
+    lights = lightTester(N)
+    instructions = parse_file(file) 
+    for command in instructions:
+        lights.apply(cmd)
+    
+    print("The number occupied : ", light.count())
+
+if __name__ == '__main__':
+    main()
+    
+class lightTester:
+    lights=None
+    
+    def __init__(self,N):
+        self.lights = ''
+    
+    def apply(self,cmd):
+        if cmd == "turn on":
+            pass
+        elif cmd == "turn off":
+            pass
+        elif cmd == "switch":
+            pass
+        else:
+            print("Invalid command!")
+            
+    def count(self):
+        pass
+        return count
+    
