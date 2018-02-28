4cffa1df426f6445aceb34a71e3724b361b0252d
oleathlc <33549380+oleathlc@users.noreply.github.com>
2018-02-28 16:15:35 +0000

    Added some code to the readFile function and tests for it too. Tests work for that function and gives an error if the http address provided is invalid

--- solve_led_project/solve_led_project.py
+++ solve_led_project/solve_led_project.py
@@ -2,35 +2,55 @@
 
 """Main module."""
 import sys
+import urllib.request
 
-def main(file, N):
+def main():
     lights = lightTester(N)
-    instructions = parse_file(file) 
-    for command in instructions:
-        lights.apply(cmd)
+    file = checkArgs()
+    if file == None:
+        return "Error"
+    else:
+        instructions = readFile(file) 
+        for command in instructions:
+            lights.apply(cmd)
     
-    print("The number occupied : ", light.count())
+            return "The number occupied : " + light.count()
 
 if __name__ == '__main__':
     main()
+
     
+def checkArgs():
+    if len(sys.argv)<3:
+        return
+    elif len(sys.argv)>3:
+        return
+    else:
+        args=str(sys.argv)
+        return args[2]
+        
 def readFile(file):
     # Rough idea of how file should be read. Will likely
     # need to put it elsewhere
-    commandList = open(file, “r”)
-    commandList.read() 
+    commandList=""
+    if file.startswith("http://"):
+        url=urllib.request.urlopen(file)
+        commandList=url.read().decode('utf-8')
+    else:
+        commandList = open(file, 'r')
+        commandList.read() 
     return commandList
 
 def getCoordinates(list):
-    #read first line of the commandList to read the first line
+    #read first line of the commandList to read the number of lines
     pass
     
     
-'''class lightTester():
-    lights=None
+class lightTester():
+    lights=[]
     
     def __init__(self,N):
-        self.lights = ''
+        self.lights = [[False]*size for _ in range(size)]
     
     def apply(self,cmd):
         if cmd == "turn on":
@@ -45,4 +65,3 @@ def getCoordinates(list):
     def count(self):
         pass
         return count
-    '''
--- tests/test_solve_led_project.py
+++ tests/test_solve_led_project.py
@@ -6,14 +6,20 @@
 import pytest
 from click.testing import CliRunner
 
-import solve_led_project
-from solve_led_project import cli
-from solve_led_project import readFile
+from solve_led_project.solve_led_project import readFile
 
-def test_readCheck():
-    pass
+def test_checkReadUrl():
+    testFile = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
+    assert readFile(testFile)!=None
+
+def test_checkReadFile():
+    test_file = 'lineTest.txt'
+    assert readFile(test_file)!= None
 
 def test_checkCoordinates():
+    pass
+
+def test_checkArgs():
     pass
 
 
